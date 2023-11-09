c09fc119fa75645656c0d4d8a2f14f105ce6b963
wdhere <wdhere@163.com>
2023-11-09 15:29:30 +0800

    feat: individual listing page

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -10,8 +10,8 @@ const Avatar: React.FC<AvatarProps> = ({ src }) => {
   return (
     <Image
       className="rounded-full"
-      height="40"
-      width="40"
+      height="30"
+      width="30"
       alt="Avatar"
       src={src || "/images/placeholder.jpg"}
     />
