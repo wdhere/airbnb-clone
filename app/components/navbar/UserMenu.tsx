546e1fb1d4cfe306c2a9da0caaa5d52b0a2ab3ee
wdhere <wdhere@163.com>
2023-11-10 17:59:22 +0800

    feat: reservations page

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -81,7 +81,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                   label="My favorites"
                 />
                 <MenuItem
-                  onClick={() => {}}
+                  onClick={() => router.push("/reservations")}
                   label="My reservations"
                 />
                 <MenuItem
---
+++ app/reservations/ReservationsClient.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+import { SafeReservation, SafeUser } from "../types";
+
+import Container from "../components/Container";
+import Heading from "../components/Heading";
+import ListingCard from "../components/listings/ListingCard";
+
+interface ReservationsClientProps {
+  reservations: SafeReservation[];
+  currentUser: SafeUser | null;
+}
+
+const ReservationsClient: React.FC<ReservationsClientProps> = ({
+  reservations,
+  currentUser,
+}) => {
+  const router = useRouter();
+  const [deletingId, setDeletingId] = useState("");
+
+  const onCancel = useCallback(
+    (id: string) => {
+      setDeletingId(id);
+
+      axios
+        .delete(`/api/reservations/${id}`)
+        .then(() => {
+          toast.success("Reservation cancelled");
+          router.refresh();
+        })
+        .catch((error) => {
+          toast.error(error?.response?.data?.error);
+        })
+        .finally(() => {
+          setDeletingId("");
+        });
+    },
+    [router]
+  );
+
+  return (
+    <Container>
+      <Heading
+        title="Reservations"
+        subtitle="Bookings on your properties"
+      />
+      <div
+        className="
+          mt-10
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6 
+          gap-8
+        "
+      >
+        {reservations.map((reservation) => (
+          <ListingCard
+            key={reservation.id}
+            currentUser={currentUser}
+            data={reservation.listing}
+            reservation={reservation}
+            actionId={reservation.id}
+            onAction={onCancel}
+            actionLabel="Cancel guest reservation"
+            disabled={deletingId === reservation.id}
+          />
+        ))}
+      </div>
+    </Container>
+  );
+};
+export default ReservationsClient;
